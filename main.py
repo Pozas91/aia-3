2b291dbcc7868c07ecc29caa8853020dccbc7304
Nicolás Pozas García <pozas91@gmail.com>
2018-02-28 20:29:14 +0100

    Unkown math range error

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import utils
+from clasificadores.perceptron_umbral import ClasificadorPU
 from clasificadores.regresion_error_cuadratico_batch import ClasificadorRECB
 from clasificadores.regresion_error_cuadratico_estocastica import ClasificadorRECE
-from clasificadores.perceptron_umbral import ClasificadorPU
+from clasificadores.regresion_verosimilitud_batch import ClasificadorRVB
 from datasets.votos import clases, entrenamiento, clases_entrenamiento, validacion, clases_validacion, test, \
     clases_test, ejemplo, ejemplo_clase
 
@@ -19,23 +20,29 @@ start_time = utils.comienzo_tiempo_ejecucion()
 clasificadorPU = ClasificadorPU(clases)
 clasificadorRECB = ClasificadorRECB(clases)
 clasificadorRECE = ClasificadorRECE(clases)
+clasificadorRVB = ClasificadorRVB(clases)
 
 # =============================================================================
 # ENTRENAMIENTO CLASIFICADORES
 # =============================================================================
 
 n_epochs = 10
-clasificadorPU.entrena(entrenamiento, clases_entrenamiento, n_epochs)
+# clasificadorPU.entrena(entrenamiento, clases_entrenamiento, n_epochs)
 
 # Si existen pesos anteriores, los recuperará, si no serán 0.
 pesos_iniciales_recb = utils.recuperar_pesos(clasificadorRECB.fichero_de_volcado)
 n_epochs = 10
-clasificadorRECB.entrena(entrenamiento, clases_entrenamiento, n_epochs, pesos_iniciales=pesos_iniciales_recb)
+# clasificadorRECB.entrena(entrenamiento, clases_entrenamiento, n_epochs, pesos_iniciales=pesos_iniciales_recb)
 
 # Si existen pesos anteriores, los recuperará, si no serán 0.
 pesos_iniciales_rece = utils.recuperar_pesos(clasificadorRECE.fichero_de_volcado)
 n_epochs = 10
-clasificadorRECE.entrena(entrenamiento, clases_entrenamiento, n_epochs, pesos_iniciales=pesos_iniciales_rece)
+# clasificadorRECE.entrena(entrenamiento, clases_entrenamiento, n_epochs, pesos_iniciales=pesos_iniciales_rece)
+
+# Si existen pesos anteriores, los recuperará, si no serán 0.
+pesos_iniciales_rvb = utils.recuperar_pesos(clasificadorRVB.fichero_de_volcado)
+n_epochs = 10
+clasificadorRVB.entrena(entrenamiento, clases_entrenamiento, n_epochs, pesos_iniciales=pesos_iniciales_rvb)
 
 # =============================================================================
 # CLASIFICACION DE EJEMPLOS
@@ -55,6 +62,14 @@ porcentaje_exito = utils.ponderar_probabilidad(probabilidad)
 clasificado = "Clasificador RECE ha clasificado a: '{0}' con una seguridad del {1:2f}%".format(clase_probable, porcentaje_exito)
 print(clasificado)
 
+
+probabilidad = clasificadorRVB.clasifica_prob(ejemplo)
+clase_probable = clasificadorRVB.clases[round(probabilidad)]
+porcentaje_exito = utils.ponderar_probabilidad(probabilidad)
+
+clasificado = "Clasificador RVB ha clasificado a: '{0}' con una seguridad del {1:2f}%".format(clase_probable, porcentaje_exito)
+print(clasificado)
+
 # =============================================================================
 # EVALUAMOS EL MODELO
 # =============================================================================
@@ -65,6 +80,10 @@ print(evaluado)
 
 rendimiento = clasificadorRECE.evalua(conjunto_prueba=test, clases_conjunto_prueba=clases_test)
 evaluado = "Rendimiento Clasificador RECE Prob.: {0:.1f}%".format(round(rendimiento * 100, 1))
+print(evaluado)
+
+rendimiento = clasificadorRVB.evalua(conjunto_prueba=test, clases_conjunto_prueba=clases_test)
+evaluado = "Rendimiento Clasificador RVB Prob.: {0:.1f}%".format(round(rendimiento * 100, 1))
 print(evaluado)
 
 # =============================================================================
