f4b8d28ff4a90527f575bcefaffcddf09b4a1615
Nicolás Pozas García <pozas91@gmail.com>
2018-02-28 11:38:02 +0100

    basic utils

--- utils.py
+++ utils.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
-from collections import Counter
 import time
 import math
-import sys
-from nodo import NodoDT
 
 """
 Función utilizada para capturar el momento en el que comienza todo.
@@ -26,3 +22,51 @@ Función utilizada para calcular los tiempos de ejecución de la aplicación en 
 def tiempo_ejecucion_obtenido(start_time):
     # Tiempo de ejecución obtenido
     print("Tiempo de ejecución en segundos: --- %s seconds ---" % (time.time() - start_time))
+
+
+"""
+La función umbral devuelve 1 si x es mayor o igual que 0, en caso contrario, devuelve 0.
+"""
+
+
+def umbral(x: float) -> int:
+    return 1 if x >= 0 else 0
+
+
+"""
+Devuelve la suma del resultado de multiplicar cada valor del atributo con su peso correspondiente.
+"""
+
+
+def pesos_por_atributo(w: list, x: list) -> float:
+    if len(w) != len(x):
+        raise ValueError("El tamaño de w y de x debe ser el mismo.")
+
+    return sum([a * b for a, b in zip(x, w)])
+
+
+"""
+Devuelve la función sigma en z.
+"""
+
+
+def sigma(z: float) -> float:
+    return 1 / (1 + math.exp(z))
+
+
+"""
+Devuelve la derivida de sigma en z.
+"""
+
+
+def derivada_sigma(z: float) -> float:
+    return sigma(z) * (1 - sigma(z))
+
+
+"""
+Devuelve la tasa de aprendizaje dado el número de epoch en que se encuentra.
+"""
+
+
+def rate_decay(tasa_inicial: float, epoch: int) -> float:
+    return tasa_inicial + 2 / ((epoch ** 2) ** (1 / 3))
