2b1352afdc89c1d0e4677ebc9b73f57590e9bf5f
Nicolás Pozas <pozas91@gmail.com>
2018-03-05 19:53:39 +0100

    Digitdata parse done!

---
+++ datasets/digitdata.py
@@ -0,0 +1,57 @@
+matrix_dimensions = 28
+max_length_index = matrix_dimensions ** 2
+
+
+# Extract formatted data from files indicated
+def extract_data(images_file: str, labels_file: str) -> (list, list):
+    data = []
+    number = []
+    classes = []
+
+    with open(images_file) as file:
+
+        lines = file.readlines()
+        index = 0
+
+        for line in lines:
+
+            # Foreach character in line without last '\n'
+            for character in line[:-1]:
+
+                # Depends of character assign a number (0 to blank, 1 to plus, and 2 to others)
+                if character == ' ':
+                    number.append(0)
+                elif character == '+':
+                    number.append(1)
+                else:
+                    number.append(2)
+
+                index += 1
+
+                if index >= max_length_index:
+                    # Add the number information to training data
+                    data.append(number.copy())
+
+                    # Reset index and number data
+                    index = 0
+                    number.clear()
+
+    with open(labels_file) as file:
+
+        lines = file.readlines()
+
+        for line in lines:
+            number = line[:-1]
+            classes.append(number)
+
+    if len(data) != len(classes):
+        raise ValueError("Numbers and labels have different size")
+
+    return data, classes
+
+
+training_data, training_classes = extract_data('digitdata/trainingimages', 'digitdata/traininglabels')
+
+test_data, test_classes = extract_data('digitdata/testimages', 'digitdata/testlabels')
+
+validation_data, validation_classes = extract_data('digitdata/validationimages', 'digitdata/validationlabels')
