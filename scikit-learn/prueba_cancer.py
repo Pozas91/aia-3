d08f244d04590d8287164e0fac1ac9dc17879a40
David Fernández <davidfernanrod@gmail.com>
2018-03-12 18:51:20 +0100

    Tarea 2 - Cáncer de mama completado

--- scikit-learn/prueba_cancer.py
+++ scikit-learn/prueba_cancer.py
@@ -7,6 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 from utils import *
@@ -40,17 +41,23 @@ Xn_train = normalizador.transform(X_train)
 # =============================================================================
 # ALGORITMO KNN
 # =============================================================================
-knn = KNeighborsClassifier(n_neighbors=11)
+knn = KNeighborsClassifier()
 
+param_grid={
+            'n_neighbors' : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+            'algorithm' : ['auto', 'ball_tree', 'kd_tree', 'brute']
+           }
+
+clf_knn = GridSearchCV(knn, param_grid)
 # Método fit
-knn.fit(Xn_train,y_train)
+clf_knn.fit(Xn_train,y_train)
 
 # Método predict
 Xn_test = normalizador.transform(X_test)
-knn.predict(Xn_test)
+clf_knn.predict(Xn_test)
 
 # Método score
-score_knn = knn.score(Xn_test,y_test)
+score_knn = clf_knn.score(Xn_test,y_test)
 
 
 # =============================================================================
@@ -58,71 +65,134 @@ score_knn = knn.score(Xn_test,y_test)
 # =============================================================================
 lr = linear_model.LinearRegression()
 
+param_grid={
+            'fit_intercept' : [True, False], 
+            'normalize': [True, False]
+            }
+
+clf_lr = GridSearchCV(lr, param_grid)
+
 # Método fit
-lr.fit(Xn_train,y_train)
+clf_lr.fit(Xn_train,y_train)
 
 # Método predict
 Xn_test = normalizador.transform(X_test)
-lr.predict(Xn_test)
+clf_lr.predict(Xn_test)
 
 # Método score
-score_lr = lr.score(Xn_test,y_test)
+score_lr = clf_lr.score(Xn_test,y_test)
 
 
 # =============================================================================
 # ALGORITMO ÁRBOL DE DECISIÓN
 # =============================================================================
-tree = DecisionTreeClassifier(criterion = "gini", random_state = 100,
-                               max_depth=5, min_samples_leaf=5)
+tree = DecisionTreeClassifier()
+
+param_grid={
+            'criterion' : ['gini', 'entropy'],
+            'random_state' : [25,50,80,100],
+            'max_depth' : [2,3,4,5],
+            'min_samples_leaf' : [1,2,3,4,5]
+            }
+
+clf_tree = GridSearchCV(tree, param_grid)
 
 # Método fit
-tree.fit(X_train, y_train)
+clf_tree.fit(Xn_train, y_train)
 
 # Método predict
 Xn_test = normalizador.transform(X_test)
-tree.predict(Xn_test)
+clf_tree.predict(Xn_test)
 
 # Método score
-score_tree = tree.score(Xn_test,y_test)
+score_tree = clf_tree.score(Xn_test,y_test)
 
 
 # =============================================================================
 # ALGORITMO RANDOM FOREST
 # =============================================================================
-random_forest = RandomForestClassifier(max_depth=2, random_state=0)
+random_forest = RandomForestClassifier()
+
+param_grid={
+            'random_state' : [25,50,80,100],
+            'max_depth' : [2,3,4,5],
+            'min_samples_leaf' : [1,2,3,4,5]
+            }
+
+clf_random_forest = GridSearchCV(random_forest, param_grid)
 
 # Método fit
-random_forest.fit(X_train, y_train)
+clf_random_forest.fit(Xn_train, y_train)
 
 # Método predict
 Xn_test = normalizador.transform(X_test)
-random_forest.predict(Xn_test)
+clf_random_forest.predict(Xn_test)
 
 # Método score
-score_random_forest = random_forest.score(Xn_test,y_test)
+score_random_forest = clf_random_forest.score(Xn_test,y_test)
 
 
 # =============================================================================
 # ALGORITMO LINEAR SVC
 # =============================================================================
-linear_svc = LinearSVC(random_state=0)
+linear_svc = LinearSVC()
+
+param_grid={
+            'random_state' : [25,50,80,100]
+        }
+
+clf_linear_svc = GridSearchCV(linear_svc, param_grid)
 
 # Método fit
-linear_svc.fit(X_train, y_train)
+clf_linear_svc.fit(Xn_train, y_train)
 
 # Método predict
 Xn_test = normalizador.transform(X_test)
-linear_svc.predict(Xn_test)
+clf_linear_svc.predict(Xn_test)
 
 # Método score
-score_linear_svc = linear_svc.score(Xn_test,y_test)
+score_linear_svc = clf_linear_svc.score(Xn_test,y_test)
+
+
+# =============================================================================
+# ALGORITMO PERCEPTRÓN
+# =============================================================================
+perceptron = linear_model.SGDClassifier()
+
+param_grid={
+            'loss' : ['hinge','log','modified_huber','squared_hinge','perceptron']
+            }
+
+clf_perceptron = GridSearchCV(perceptron, param_grid)
+
+# Método fit
+clf_perceptron.fit(Xn_train,y_train)
+
+# Método predict
+Xn_test = normalizador.transform(X_test)
+clf_perceptron.predict(Xn_test)
+
+# Método score
+score_perceptron = clf_perceptron.score(Xn_test,y_test)
 
 
 # =============================================================================
 # RESULTADOS OBTENIDOS
 # =============================================================================
 print("Porcentaje de aciertos sobre un conjunto de prueba (KNN): {0:.2f} %".format(score_knn * 100))
+print("Mejores parámetros elegidos: {0}".format(clf_knn.best_params_))
+print("*******************************************************")
 print("Porcentaje de aciertos sobre un conjunto de prueba (Linear Regression): {0:.2f} %".format(score_lr * 100))
+print("Mejores parámetros elegidos: {0}".format(clf_lr.best_params_))
+print("*******************************************************")
 print("Porcentaje de aciertos sobre un conjunto de prueba (Árbol de decisión): {0:.2f} %".format(score_tree * 100))
+print("Mejores parámetros elegidos: {0}".format(clf_tree.best_params_))
+print("*******************************************************")
 print("Porcentaje de aciertos sobre un conjunto de prueba (Random Forest): {0:.2f} %".format(score_random_forest * 100))
+print("Mejores parámetros elegidos: {0}".format(clf_random_forest.best_params_))
+print("*******************************************************")
 print("Porcentaje de aciertos sobre un conjunto de prueba (LinearSVC): {0:.2f} %".format(score_linear_svc * 100))
+print("Mejores parámetros elegidos: {0}".format(clf_linear_svc.best_params_))
+print("*******************************************************")
+print("Porcentaje de aciertos sobre un conjunto de prueba (SGDClassifier): {0:.2f} %".format(score_perceptron * 100))
+print("Mejores parámetros elegidos: {0}".format(clf_perceptron.best_params_))
